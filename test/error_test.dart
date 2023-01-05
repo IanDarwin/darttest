f67a9353e110e09526dcaae72a50e3ca3cc12208
Ian Darwin <ian@darwinsys.com>
2023-01-04 19:07:32 -0500

    Error handling tests

---
+++ test/error_test.dart
@@ -0,0 +1,35 @@
+import "package:test/test.dart";
+
+/// Test for error handling
+main() {
+  group("Error Handling Tests", () {
+    test("substring can't be too big: RangeError", () {
+      expect(() => "abc".substring(5, 5), throwsRangeError);
+    });
+    test("substring can't be negative: RangeError", () {
+      expect(() => "abc".substring(-1), throwsRangeError);
+    });
+    test("Deliberately throwing a 'MyError'", () {
+      expect(() => dummyProcess(""), throwsA(TypeMatcher<MyError>()));
+    });
+  });
+}
+
+dummyProcess(String x) {
+  if (x.length == 0) {
+    throw AssertionError("I don't like your input");
+  }
+  if (x.length == 1) {
+    throw MyError();
+  }
+  if (x == 'date') {
+    throw DateTime.now();
+  }
+  if (x == 'number') {
+    throw 1;
+  }
+  print("OK");
+}
+class MyError {
+
+}
